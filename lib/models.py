0226cf050407e49fd30272b94df68f46f484482e
kimemia <david.thuku@student.moringaschool.com>
2023-09-06 04:58:34 -0700

    Done

--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
-
-from sqlalchemy import (Column, String, Integer)
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (Column, String, Integer,create_engine)
+from sqlalchemy.orm import declarative_base,sessionmaker
 
 Base = declarative_base()
 
@@ -11,3 +10,36 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __repr__(self):
+        return f"animal {self.id}: " \
+            + f" name {self.name}, " \
+            + f"breed  {self.breed}"
+        
+
+
+
+if __name__=="__main__":
+    try:
+        engine=create_engine("sqlite:///:memory:")
+        Base.metadata.create_all(engine)
+        session=sessionmaker(bind=engine)
+        session=session()
+        
+
+        rough=Dog(name="rough",breed="German Shepard")
+        softie=Dog(name="sofie",breed="rotwailer")
+        session.add_all([rough,softie])
+        session.commit()
+
+        for animal in session.query(Dog):
+            print(animal)
+
+
+
+    except Exception as e:
+        print("got this error ", str(e))    
+
+
+
+ 
